83a87ddbe8500de8a4b6f32b92b5083846fb365d
Gunhee Yi <gunny2676@gmail.com>
2025-10-24 20:59:43 +0900

    gunhee: add property `name` to class `Planet`

--- classes/celestial-bodies/Planet.m
+++ classes/celestial-bodies/Planet.m
@@ -1,11 +1,13 @@
 classdef Planet < CelestialBody
     properties
+        name
         mu
         flybyable
     end
     methods
-        function planet = Planet(celestialBody_args, mu, flybyable)
+        function planet = Planet(celestialBody_args, name, mu, flybyable)
             planet@CelestialBody(celestialBody_args)
+            planet.name = name;
             planet.mu = mu;
             planet.flybyable = flybyable;
         end
